71c790c23b7742a50e954a682027ec733c51d3a4
miyamonz <m5813k@gmail.com>
2022-07-23 00:59:50 +0900

    new_binary

--- parse.c
+++ parse.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+Node *new_node(NodeKind kind)
 {
     Node *node = calloc(1, sizeof(Node));
     node->kind = kind;
+    return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = new_node(kind);
     node->lhs = lhs;
     node->rhs = rhs;
     return node;
@@ -11,8 +17,7 @@ Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
 
 Node *new_node_num(int val)
 {
-    Node *node = calloc(1, sizeof(Node));
-    node->kind = ND_NUM;
+    Node *node = new_node(ND_NUM);
     node->val = val;
     return node;
 }
@@ -63,9 +68,9 @@ Node *equality()
     for (;;)
     {
         if (consume("=="))
-            node = new_node(ND_EQ, node, relational());
+            node = new_binary(ND_EQ, node, relational());
         if (consume("!="))
-            node = new_node(ND_NE, node, relational());
+            node = new_binary(ND_NE, node, relational());
         else
             return node;
     }
@@ -79,13 +84,13 @@ Node *relational()
     for (;;)
     {
         if (consume("<"))
-            node = new_node(ND_LT, node, add());
+            node = new_binary(ND_LT, node, add());
         if (consume("<="))
-            node = new_node(ND_LE, node, add());
+            node = new_binary(ND_LE, node, add());
         if (consume(">"))
-            node = new_node(ND_LT, add(), node);
+            node = new_binary(ND_LT, add(), node);
         if (consume(">="))
-            node = new_node(ND_LE, add(), node);
+            node = new_binary(ND_LE, add(), node);
         else
             return node;
     }
@@ -99,9 +104,9 @@ Node *add()
     for (;;)
     {
         if (consume("+"))
-            node = new_node(ND_ADD, node, mul());
+            node = new_binary(ND_ADD, node, mul());
         if (consume("-"))
-            node = new_node(ND_SUB, node, mul());
+            node = new_binary(ND_SUB, node, mul());
         else
             return node;
     }
@@ -114,9 +119,9 @@ Node *mul()
     for (;;)
     {
         if (consume("*"))
-            node = new_node(ND_MUL, node, unary());
+            node = new_binary(ND_MUL, node, unary());
         if (consume("/"))
-            node = new_node(ND_DIV, node, unary());
+            node = new_binary(ND_DIV, node, unary());
         else
             return node;
     }
@@ -129,7 +134,7 @@ Node *unary()
     if (consume("+"))
         return unary();
     if (consume("-"))
-        return new_node(ND_SUB, new_node_num(0), unary());
+        return new_binary(ND_SUB, new_node_num(0), unary());
     return primary();
 }
 
