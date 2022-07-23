fd9c4d0d5573cfb45bbf9bb2589292f608d670ed
miyamonz <m5813k@gmail.com>
2022-07-23 22:35:54 +0900

    expr statement

--- parse.c
+++ parse.c
@@ -63,7 +63,8 @@ Node *stmt()
         expect(";");
         return node;
     }
-    Node *node = expr();
+
+    Node *node = new_unary(ND_EXPR_STMT, expr());
     expect(";");
     return node;
 }
