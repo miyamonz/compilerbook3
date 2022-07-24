1789b081ca21632ce463558818584a1eeb1a10c0
miyamonz <m5813k@gmail.com>
2022-07-24 23:10:56 +0900

    make sure that frames are aligned to 16bytes boundaries

--- codegen.c
+++ codegen.c
@@ -127,7 +127,24 @@ void gen(Node *node)
         {
             printf("  pop %s\n", argreg[i]);
         }
+        // We need to align RSP to a 16 byte boundary before
+        // calling a function because it is an ABI requirement.
+        // RAX is set to 0 for variadic function.
+
+        // pushやpopは8バイト単位で変更するので、RSPは16バイト単位で0とは限らない
+        int seq = labelseq++;
+        printf("  mov rax, rsp\n");
+        printf("  and rax, 15\n"); // ここで16の倍数であるかを確認する
+        printf("  jnz .Lcall%d\n", seq);
+        printf("  mov rax, 0\n");
         printf("  call %s\n", node->funcname);
+        printf("  jmp .Lend%d\n", seq);
+        printf(".Lcall%d:\n", seq);
+        printf("  sub rsp, 8\n"); // 8引いて16の倍数にしてうr
+        printf("  mov rax, 0\n");
+        printf("  call %s\n", node->funcname);
+        printf("  add rsp, 8\n"); // ずらした分を戻しておく
+        printf(".Lend%d:\n", seq);
         printf("  push rax\n");
         return;
     }
