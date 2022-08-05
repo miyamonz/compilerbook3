3dd414f957d4570c807e2ffc072e2460881e54d7
miyamonz <m5813k@gmail.com>
2022-08-05 13:00:25 +0000

    add  sizeof operator

--- parse.c
+++ parse.c
@@ -437,9 +437,11 @@ Node *func_args()
     return head;
 }
 
-// primary = "(" expr ")" | ident func-args? | num
+// primary = "(" expr ")" | "sizeof" unary | ident func-args? | num
 Node *primary()
 {
+    Token *tok;
+
     if (consume("("))
     {
         Node *node = expr();
@@ -447,7 +449,9 @@ Node *primary()
         return node;
     }
 
-    Token *tok;
+    if (tok = consume("sizeof"))
+        return new_unary(ND_SIZEOF, unary(), tok);
+
     if (tok = consume_ident())
     {
         // function call
--- tokenize.c
+++ tokenize.c
@@ -138,7 +138,7 @@ bool is_alnum(char c)
 char *starts_with_reserved(char *p)
 {
     // Keyword
-    static char *kw[] = {"return", "if", "else", "while", "for", "int"};
+    static char *kw[] = {"return", "if", "else", "while", "for", "int", "sizeof"};
 
     for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
     {
