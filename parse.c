cc6feb879c82a23d282d24b91939a28b9c0d7185
miyamonz <m5813k@gmail.com>
2022-10-17 02:23:53 +0900

    add -> operator

--- parse.c
+++ parse.c
@@ -582,7 +582,7 @@ Node *unary()
     return postfix();
 }
 
-// postfix = primary ("[" expr "]" | "." ident)*
+// postfix = primary ("[" expr "]" | "." ident | "->" ident)*
 Node *postfix()
 {
     Node *node = primary();
@@ -600,6 +600,14 @@ Node *postfix()
         }
         if (tok = consume("."))
         {
+            node = new_unary(ND_MEMBER, node, tok);
+            node->member_name = expect_ident();
+            continue;
+        }
+        if (tok = consume("->"))
+        {
+            // x->y is short for (&x).y
+            node = new_unary(ND_DEREF, node, tok);
             node = new_unary(ND_MEMBER, node, tok);
             node->member_name = expect_ident();
             continue;
