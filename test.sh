eaf0f9be719b25e024f37b42dba27a8fa64c4102
miyamonz <m5813k@gmail.com>
2023-02-21 02:35:25 +0900

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# https://gist.github.com/maekawatoshiki/bc5cd710aee4b29e47b4f3c4384bcb39
+
+unique() {
+  echo $RANDOM | md5sum | head -c 10
+}
+
+LOG=/tmp/log$(unique)
+
+
 cat <<EOF | gcc -xc -c -o tmp2.o -
 int ret3() { return 3; }
 int ret5() { return 5; }
@@ -10,20 +19,38 @@ int add6(int a, int b, int c, int d, int e, int f) {
 }
 EOF
 
-assert() {
+do_assert() {
   expected="$1"
   input="$2"
 
-  ./main <(echo "$input") > tmp.s
-  cc $( [[ -v STATIC ]] && echo '-static') -o tmp tmp.s tmp2.o
-  ./tmp
+  UNIQ=$(unique)
+
+  ./main <(echo "$input") > /tmp/tmp$UNIQ.s
+  cc $( [[ -v STATIC ]] && echo '-static') -o /tmp/tmp$UNIQ /tmp/tmp$UNIQ.s tmp2.o
+  /tmp/tmp$UNIQ
   actual="$?"
 
   if [ "$actual" = "$expected" ]; then
     echo "$input => $actual"
   else
-    echo "$input => $expected expected, but got $actual"
+    # echo "$input => $expected expected, but got $actual"    
+    printf "\033[31m$input => $expected expected, but got $actual\033[0m\n" | tee $LOG
     exit 1
+  fi
+}
+
+assert() {
+  do_assert "$1" "$2" &
+}
+
+check() {
+  if [ ! -f $LOG ]; then
+    printf "\033[32mOK\033[0m\n"
+    return 0
+  else
+    FAILS=$(cat $LOG | wc -l)
+    printf "\033[31m$FAILS tests failed\033[0m\n"
+    return 1
   fi
 }
 
@@ -51,4 +78,6 @@ assert 2 'int main() { /* return 1; */ return 2; }'
 assert 2 'int main() { // return 1;
 return 2; }'
 
-echo OK
+wait 
+
+check
